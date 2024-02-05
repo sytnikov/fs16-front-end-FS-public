93ee80ae4680f0deb03e2f41a9e47ffdaa6bb702
sytnikov <sytnikov.alexey@gmail.com>
2024-02-05 16:03:55 +0200

    updated backend server link and .gitignore file

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,2 +1,2 @@
-export const baseURL = process.env.NODE_ENV === 'production' ? 'https://fullstack-backend-juzm.onrender.com' : 'http://localhost:8080'
+export const baseURL = process.env.NODE_ENV === 'production' ? 'https://tennis-store-backend.onrender.com/' : 'http://localhost:8080'
 export const testURL = 'http://localhost:8080'
